955c92b79f7262d3709c18e98ce0d84f1c633175
Forshteven <geocinds@mail.ru>
2024-10-13 11:16:44 +0300

    Add files via upload

--- module_7_3.py
+++ module_7_3.py
@@ -7,8 +7,9 @@ class WordsFinder:
         for file_name in self.file_names:
             try:
                 with open(file_name, 'r', encoding='utf-8') as file:
-                    words = file.read().split().lower()
-                    words_dict[file_name] = words
+                    words = file.read().split()
+                    words = [word.lower() for word in words]
+                    all_words[file_name] = words
             except FileNotFoundError:
                 print(f'Файл {file_name} не найден.')
             except Exception as e:
@@ -18,16 +19,18 @@ class WordsFinder:
     def find(self, word):
         result = {}
         all_words = self.get_all_words()  # Получаем все слова
-
+        word = word.lower()
         for file_name, words in all_words.items():
             if word in words:
                 result[file_name] = words.index(word)
+            else:
+                print(f"Слово '{word}' не найдено в файле {file_name}.")
             return result
 
     def count(self, word):
         result = {}
         all_words = self.get_all_words()  # Получаем все слова
-
+        word = word.lower()
         for file_name, words in all_words.items():
             count = words.count(word)  # Считаем количество вхождений
             if count > 0:
@@ -39,7 +42,7 @@ class WordsFinder:
         return f'WordsFinder(file_names = {self.file_names})'
 
 
-finder2 = WordsFinder('test_file.txt')
+finder2 = WordsFinder('Mother Goose - Monday’s Child.txt')
 print(finder2.get_all_words())  # Все слова
-print(finder2.find('TEXT'))  # 3 слово по счёту
-print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
+print(finder2.find('child'))  # 3 слово по счёту
+print(finder2.count('child'))  # 4 слова teXT в тексте всего
